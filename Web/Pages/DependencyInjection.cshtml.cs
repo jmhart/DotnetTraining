e7e4cbe3501bcb95f0f0bbe932c16a0075056c6a
jeffrey.m.hart <jeffrey.m.hart@avanade.com>
2023-11-07 15:21:20 -0800

    Dependency injection example

--- Web/Pages/DependencyInjection.cshtml.cs
+++ Web/Pages/DependencyInjection.cshtml.cs
@@ -1,13 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using Infra.Data;
+
 namespace Web.Pages
 {
     public class DependencyInjectionModel : PageModel
     {
+        public ExampleDbContext Context { get; }
+        public List<ExampleEntity> Entities { get; set; }
+
+        public DependencyInjectionModel(ExampleDbContext context) 
+        {
+            Context = context;
+        }
+
         public void OnGet()
         {
             ViewData["Title"] = "Dependency Injection";
+            Entities = Context.ExampleEntities.ToList();
         }
     }
 }
