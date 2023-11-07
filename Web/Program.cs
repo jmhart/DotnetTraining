b5e34f391acb7e3b96e7362ff78c6670ba368d42
jeffrey.m.hart <jeffrey.m.hart@avanade.com>
2023-11-07 15:20:59 -0800

    Add Infra references

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<Infra.Data.ExampleDbContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
