210d81f16d0c307188221c6e54ab1ad7d2366bbe
jeffrey.m.hart <jeffrey.m.hart@avanade.com>
2023-11-08 15:46:34 -0800

    Validation example

--- Web/Pages/Validation.cshtml.cs
+++ Web/Pages/Validation.cshtml.cs
@@ -1,13 +1,50 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace Web.Pages
 {
     public class ValidationModel : PageModel
     {
+        [BindProperty]
+        public PizzaOrder PizzaOrder { get; set; }
+
         public void OnGet()
         {
             ViewData["Title"] = "Validation";
         }
+
+        public void OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return;
+            }
+            // Process the form
+        }
+    }
+
+    // TODO: Move classes to Domain.Entities.
+    public class PizzaOrder
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public PizzaSize Size { get; set; }
+        public IList<Topping> Toppings { get; set; }
+    }
+
+    public class Topping {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public enum PizzaSize {
+        Small,
+        Medium,
+        Large
     }
 }
