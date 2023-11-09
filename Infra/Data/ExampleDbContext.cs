d450a5639761893e538619f21ffe0cdf15d9ec46
jeffrey.m.hart <jeffrey.m.hart@avanade.com>
2023-11-09 12:59:12 -0800

    Example dbcontext test

--- Infra/Data/ExampleDbContext.cs
+++ Infra/Data/ExampleDbContext.cs
@@ -14,6 +14,7 @@ namespace Infra.Data
     public class ExampleEntity
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+
+        public string? Name { get; set; }
     }
 }
